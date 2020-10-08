79181e1b7c0bb43a0657d328ba39d80421f6aace
richy163 <ritchycal@gmail.com>
2020-10-08 16:27:49 -0400

    done part 2

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,31 +1,74 @@
-import React, { Component } from 'react';
-import './App.css';
-
-
+import React, { Component } from "react";
+import "./App.css";
 function City(props) {
-  return (<div>This is the City component</div>);
+	return (
+		<div className="card">
+			<h3>
+				{props.data.City}, {props.data.State}
+			</h3>
+			<ul className="locItem">
+				<li>Population: {props.data.EstimatedPopulation}</li>
+				<li>
+					Location: ({props.data.Lat}, {props.data.Long})
+				</li>
+				<li>TotalWages: ({props.data.TotalWages})</li>
+			</ul>
+		</div>
+	);
 }
-
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+	return (
+		<div className="input-field">
+			<div className="lable">Zip Code: </div>{" "}
+			<input
+				type="text"
+				onChange={(e) => props.zipChanged(e)}
+				value={props.value}
+			/>
+		</div>
+	);
 }
-
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Zip Code Search</h2>
-        </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
-        </div>
-      </div>
-    );
-  }
+	state = {
+		inputZip: "",
+		cityResults: [],
+	};
+	handleZipChange(e) {
+		this.setState({
+			inputZip: e.target.value,
+		});
+		if (e.target.value.length === 5) {
+			fetch("https://ctp-zip-api.herokuapp.com/zip/" + e.target.value)
+				.then((res) => res.json())
+				.then((jsonData) => {
+					this.setState({
+						cityResults: jsonData,
+					});
+				})
+				.catch((err) => {
+					this.setState({ cityResults: [] });
+				});
+		} else {
+			this.setState({ cityResults: [] });
+		}
+	}
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<h2>Zip Code Search</h2>
+				</div>
+				<ZipSearchField
+					zipChanged={(e) => this.handleZipChange(e)}
+					value={this.state.inputZip}
+				/>
+				<div>
+					{this.state.cityResults.map((item, index) => {
+						return <City data={item} key={index} />;
+					})}
+				</div>
+			</div>
+		);
+	}
 }
-
 export default App;
